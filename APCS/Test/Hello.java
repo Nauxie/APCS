af8cd080233a675693741f311b61ad29c1439b66
Nauxie <abhinav.tumu1@gmail.com>
2018-08-29 18:44:05 -0700

    tested casting in hello.java

--- APCS/Test/Hello.java
+++ APCS/Test/Hello.java
@@ -1,4 +1,14 @@
 
 public class Hello {
-
+	public static void main(String[] args) {
+		double a = 3.6;
+		double b = 4;
+		double c = 5;
+		int d = 6;
+		int e = 7;
+		int f =  (int) (a + b + c + d + e);
+		double g = (a+b+c+d+e);
+		System.out.println(f);
+		System.out.println(g);
+	}
 }
