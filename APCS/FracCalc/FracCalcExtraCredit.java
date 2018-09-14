dec0b494e09e447dcf9966cbe71c656801786a85
Nauxie <abhinav.tumu1@gmail.com>
2018-09-13 20:29:07 -0700

    worked on EC (no progress)

--- APCS/FracCalc/FracCalcExtraCredit.java
+++ APCS/FracCalc/FracCalcExtraCredit.java
@@ -1,40 +1,26 @@
 
 public class FracCalcExtraCredit {
 	public static void main(String[] args) {
-		System.out.println(produceAnswer("5_3/4 - -6_8/8 - 5_3/4"));
+		System.out.println(produceAnswer("5_3/4 - -6_7/8"));
 	}
 	public static String produceAnswer(String calc) {
-		
 		String splitUp[] = calc.split(" "); 
+		int splitUpLength = splitUp.length;
+		//String splitUpEC[] = calc.split("[ _/]"); 
+		//integer calcLength = splitUpEC.length;
 		String finalAnswer = "445"; //assigns recognition value to return string
 		if (splitUp.length == 3) {
 		}
-		else {
-			int calcLength = splitUp.length;
-			//finalAnswer = calcLength + "";
-			String a = "0";
-			String b = "0";
-			String c = "1";
-			String lastFrac = splitUp[calcLength-1];
-			if (lastFrac.contains("_")) {
-				String splitLastFrac[] = lastFrac.split("_");
-				String multiSplitLastFrac[] = splitLastFrac[1].split("/");
-				a = splitLastFrac[0];
-				b = multiSplitLastFrac[0];
-				c = multiSplitLastFrac[1];
-				finalAnswer = "whole:" + a + " numerator:" + b + " denominator:" + c;
-			} else if(lastFrac.contains("/") && !(lastFrac.contains("_"))) {
-				String splitLastFrac[] = lastFrac.split("/");
-				b = splitLastFrac[0];
-				c = splitLastFrac[1];
-				finalAnswer = "whole:" + a + " numerator:" + b + " denominator:" + c;
+		else {	
+			String[] finalArray = new String[4*splitUpLength - 5];
+			for(int i = 2; i <= finalArray.length; i += 4) {
+				finalArray[i] = "1";
+				
 			}
-			else {
-				a = lastFrac;
-				finalAnswer = "whole:" + a + " numerator:" + b + " denominator:" + c;
-			}			
-		}
+			
 		
+					
+		} 
 		
 		
 		
