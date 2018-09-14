1af27b51cc41852f6a436deb2ed644ba05ad380e
Nauxie <abhinav.tumu1@gmail.com>
2018-09-13 22:03:07 -0700

    worked on EC

--- APCS/FracCalc/FracCalcExtraCredit.java
+++ APCS/FracCalc/FracCalcExtraCredit.java
@@ -1,29 +1,48 @@
 
 public class FracCalcExtraCredit {
 	public static void main(String[] args) {
-		System.out.println(produceAnswer("5_3/4 - -6_7/8"));
+		System.out.println(produceAnswer("5_3/4 * 6_7/8 * 3/4"));
 	}
-	public static String produceAnswer(String calc) {
-		String splitUp[] = calc.split(" "); 
-		int splitUpLength = splitUp.length;
-		//String splitUpEC[] = calc.split("[ _/]"); 
-		//integer calcLength = splitUpEC.length;
-		String finalAnswer = "445"; //assigns recognition value to return string
-		if (splitUp.length == 3) {
+
+	public static boolean isNum(String strNum) {
+		boolean ret = true;
+		try {
+
+			Integer.parseInt(strNum);
+
+		} catch (NumberFormatException e) {
+			ret = false;
 		}
-		else {	
-			String[] finalArray = new String[4*splitUpLength - 5];
-			for(int i = 2; i <= finalArray.length; i += 4) {
-				finalArray[i] = "1";
-				
+		return ret;
+	}
+
+	public static String produceAnswer(String calc) {
+		String splitUp[] = calc.split(" ");
+		int splitUpLength = splitUp.length;
+		// String splitUpEC[] = calc.split("[ _/]");
+		// integer calcLength = splitUpEC.length;
+		String finalAnswer = "445"; // assigns recognition value to return string
+		if (splitUp.length == 3) {
+		} else {
+			String splitUpArray[] = calc.split("[ _/]");
+			int i = 0;
+			while (i <= splitUpArray.length - 1) {
+				if (isNum(splitUpArray[i + 2])) {
+					int a = Integer.parseInt(splitUpArray[i]);
+					int b = Integer.parseInt(splitUpArray[i + 1]);
+					int c = Integer.parseInt(splitUpArray[i + 2]);
+					int iNum1 = a * c + b;
+					int iDen1 = c;
+					finalAnswer = iNum1 + "" + iDen1;
+
+				} else {
+					finalAnswer = "333";
+				}
+
 			}
-			
-		
-					
-		} 
-		
-		
-		
+
+		}
+
 		return finalAnswer;
 	}
 }
