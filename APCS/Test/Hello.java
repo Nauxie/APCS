fd595f55e16be34e2c6da0550f45d9dcccd1ee41
Nauxie <abhinav.tumu1@gmail.com>
2018-09-26 22:18:27 -0700

    complted

--- APCS/Test/Hello.java
+++ APCS/Test/Hello.java
@@ -1,8 +1,24 @@
+import java.util.*;
 
 public class Hello {
 	public static void main(String[] args) {
-		String[] lengthTest = new String[6];
-		int i = lengthTest.length;
-		System.out.println(i);
+		Random rand = new Random();
+
+		int position = 0;
+		int maxPos = 0;
+		System.out.println("position = " + position);
+		while (Math.abs(position) < 10) {
+			int a = rand.nextInt(2);
+			if (a == 0) {
+				position++;
+				maxPos = Math.max(maxPos, position);
+				System.out.println("position = " + position);
+
+			} else {
+				position--;
+				System.out.println("position = " + position);
+			}
+		}
+		System.out.println("max position = " + maxPos);
 	}
 }
